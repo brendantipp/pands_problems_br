8dccfc7bc3e1da6d198e2cbb841ea73969b0b140
Brendan Ryan <brendantipp@gmail.com>
2020-02-16 18:39:10 +0000

    added stops if not positive

--- collatz.py
+++ collatz.py
@@ -1,26 +1,35 @@
 # Brendan Ryan
-#
+#progam that asks user to input a positive integer and outputs the following calculation
+#at each step calculate the next value by taking the current value and if it is even
+#divide by two if it is odd multiply by three and add one
+#have the program end if the current value is one
 
 
+#ask user to enter a positive value
 a = int(input("please enter a positive integer: "))
 
-while a < 1:
+#if value entered is negative print "not a positive number" and stop
+while a < 0:
+    print (a,"is not a positive number")
     break
 
 
-#prints value of (a) - if <1 will stop here and print value
-#print (a)
+#prints value of (a) when positive integer
+while a > 0:
+    print (a)
+    break
 
+#perform calculations until reaches 1
 
 while a > 1:
 
     if a % 2 == 0:
         a =  int(a/2)
-        #print (a) for testing
-        else:
-            a = int((a*3)+1)
+        
+    else:
+        a = int((a*3)+1)
 
-        print (a)
+    print (a)
 
 
 
