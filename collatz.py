c8f875f8b58e295066ea1951135aef128623b157
Brendan Ryan <brendantipp@gmail.com>
2020-02-16 18:15:50 +0000

    testing

--- collatz.py
+++ collatz.py
@@ -1,25 +1,26 @@
 # Brendan Ryan
+#
 
 
-a = int(input("enter an integer: "))
+a = int(input("please enter a positive integer: "))
 
-#prints value of a - if <1 will stop here and print value
-print (a)
+while a < 1:
+    break
 
-#continues if value of a is greater than 1
+
+#prints value of (a) - if <1 will stop here and print value
+#print (a)
+
 
 while a > 1:
 
     if a % 2 == 0:
-       a =  int(a/2)
-       #print (a) for testing
-    else:
-        a = int((a*3)+1)
+        a =  int(a/2)
+        #print (a) for testing
+        else:
+            a = int((a*3)+1)
 
-    print (a)
-
-
-
+        print (a)
 
 
 
