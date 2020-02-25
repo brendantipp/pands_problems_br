cf1b97ce7da7d5e4dd9aecf995b8dc15283f00ad
Brendan Ryan <brendantipp@gmail.com>
2020-02-25 21:52:05 +0000

    tested version

--- weekday.py
+++ weekday.py
@@ -2,19 +2,27 @@
 #Return messsage to user based on day being weekday or weekend
 
 
+
+#import the date time phython library
 import datetime
 
+#extract todays date and time etc
 today = datetime.datetime.now()
 
+#extract today index
 day = today.weekday()
+
+#print(day) testing only
+
+#created dict for day names
 dayname ={0:'Monday', 1:'Tuesday', 2:'Wednesday', 3:'Thursday', 4:'Friday', 5:'Saturday', 6:'Sunday'}
 
 print("Today is ... ", dayname[day])
 
-#print(days)
+#print next line based on day index extracted
 
 if day < 4:
-    print("boo its a weekday")
+    print("boo its a weekday no fun")
 else:
     print("yes its the weekend")
 
