f82b47d55438270e6896584eced475794ffc26c0
Brendan Ryan <brendantipp@gmail.com>
2020-03-07 13:55:06 +0000

    inital upload

---
+++ brendan_testing/brendan_months.py
@@ -0,0 +1,28 @@
+##testing
+
+months =  ("january","february", "march", "april", "May", "June", 
+"July", "August", "September", "October", "November", "December")
+
+summer = months[4:7]
+
+print (summer)
+
+for month in summer:
+    print (month)
+
+
+
+for darkness in summer:
+    print (darkness)
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ es.py
@@ -0,0 +1,7 @@
+## Brendan Ryan
+## March 2020
+
+with open('premiership.txt', 'r') as brendan_file:
+
+    for line in brendan_file.readlines():
+        print(line, end='')
