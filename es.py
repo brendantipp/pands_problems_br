2911af1855f3415c60875baba326ea46e5422e4f
Brendan Ryan <brendantipp@gmail.com>
2020-03-07 16:00:46 +0000

    final works added comments and description

--- es.py
+++ es.py
@@ -1,16 +1,32 @@
-## Brendan Ryan
+## Brendan Ryan - Task 7
 ## March 2020
+## Description - Program that reads in a text file as inputted and retruns the count of the characters 
+# The user is requested to input the filename and character they wish to find 
+#
 
-#import math
+#ask user to input the file name
+filename = (input("Enter the file name:"))
+
+#ask user to input the character they wish to find
+character = (input("Enter character you wish to find:"))
+
+#Open file as read and define as result
+
+with open(filename, 'r') as file:
+
+    result= file.read()
+
+#output the result
+
+print("There is:",result.count(character),"",character,"s in that file")
 
 
 
-with open('premiership.txt', 'r') as brendan_file:
 
-    for line in brendan_file.readlines():
-        line = "e"
-        count = line.count(line)
-    print("The count is:", count)
+
+
+    
+
 
 
 
