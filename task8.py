14113e9a3334506ccde8fe6cbc637b3ce2b5aa81
Brendan Ryan <brendantipp@gmail.com>
2020-03-21 15:17:20 +0000

    first commit

---
+++ task8.py
@@ -0,0 +1,36 @@
+#task 8 brendan ryan
+# march 17th 2020
+# Write a program that displays 
+# a plot of the functions f(x)=x, g(x)=x2 and h(x)=x3 in the range [0, 4] 
+# on the one set of axes. 
+
+##import librarys required
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+##create definitions
+def f(x):
+    return x
+
+def g(x):
+    return x**2
+
+def h(x):
+    return x**3
+
+#the range required 
+x = np.arange(0,4)
+
+##plotting the results
+
+plt.plot (f(x),g(x),h(x))
+
+plt.show()
+
+
+
+
+
+
+
