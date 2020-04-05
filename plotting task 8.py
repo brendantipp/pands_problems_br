da2b61472fb5b65b51b161f9d67a3f68d4568437
Brendan Ryan <brendantipp@gmail.com>
2020-04-05 16:52:18 +0100

    updated name to task 8 plots

---
+++ plotting task 8.py
@@ -0,0 +1,51 @@
+#task 8 brendan ryan
+# march 17th 2020
+# Write a program that displays 
+# a plot of the functions f(x)=x, g(x)=x2 and h(x)=x3 in the range [0, 4] 
+# on the one set of axes. 
+
+##import librarys required
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+##create definitions
+def f(x):
+    return x
+
+def g(x):
+    return x**2
+
+def h(x):
+    return x**3
+
+#the range required - up in increments of 1
+x = np.arange(0,4,1)
+
+##plotting the results
+
+plt.plot (f(x),g(x),h(x))
+plt.xlabel("x axis")
+plt.ylabel("y axis")
+plt.title("brendans graph")
+plt.show()
+
+#print (f(x),g(x),h(x))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
