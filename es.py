9d8215d22ff9fb13f2e3e1b91ab2f229e90737ee
Brendan Ryan <brendantipp@gmail.com>
2020-04-05 17:48:47 +0100

    tidy up code and coments and incl research links

--- es.py
+++ es.py
@@ -1,8 +1,10 @@
-## Brendan Ryan - Task 7
-## March 2020
+## Brendan Ryan - Task 7 - March 2020
 ## Description - Program that reads in a text file as inputted and retruns the count of the characters 
-# The user is requested to input the filename and character they wish to find 
-#
+##The user is requested to input the filename and character they wish to find
+
+#Any sources or references used :
+#https://www.includehelp.com/python/counting-the-occurrences-of-a-substring-in-a-string-using-string-count.aspx
+#https://stackoverflow.com/questions/18395587/splitting-characters-from-a-text-file-in-python
 
 #ask user to input the file name
 filename = (input("Enter the file name:"))
@@ -11,24 +13,8 @@ filename = (input("Enter the file name:"))
 character = (input("Enter character you wish to find:"))
 
 #Open file as read and define as result
-
 with open(filename, 'r') as file:
-
     result= file.read()
 
 #output the results
-
 print("There is:",result.count(character),"",character,"s in that file")
-
-
-
-
-
-
-    
-
-
-
-
-
-       
