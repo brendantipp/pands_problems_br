bead9999aa31ff8b38f20d1ffb64d9266e8d34c7
Brendan Ryan <brendantipp@gmail.com>
2020-04-05 17:12:07 +0100

    improved code after lectured review and tested

--- secondstring.py
+++ secondstring.py
@@ -1,50 +1,7 @@
-#Brendan
+#Brendan Ryan - Program that asks user to input a string and output every second work
 
 #ask the user to enter a sentence
-
 sentence = (input("Enter a string : ")) 
- 
-#reverse the sentence first
-
-sentence = sentence[::-1]
-
-#print(sentence) uncomment to check the reverse 
 
 # Now print every second word of the reversed sentence
-
-print ("This is your sentence with every second word in reverse order",sentence[::2])
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+print ("This is your sentence with every second word in reverse order",sentence[::-2])
